5f9afd4f1466681fb4a8d8455b4a2f4b0e1352d4
Artur Maslov <aartasas@gmail.com>
2018-05-05 14:26:13 +0300

    Form is fully working according to reduxform docs

--- components/CardForm.jsx
+++ components/CardForm.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import { Field } from 'redux-form';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
+import AddIcon from 'material-ui/svg-icons/content/add';
+import CancelIcon from 'material-ui/svg-icons/navigation/close';
+import { green800, green600 } from 'material-ui/styles/colors';
+import { Card, CardText, CardHeader } from 'material-ui/Card';
+
+const renderField = props => (
+  <div className="input-row">
+    <TextField
+      id={props.name}
+      {...props.input}
+      hintText={props.hint}
+      floatingLabelText={props.label}
+      multiLine
+      fullWidth
+      rows={1}
+      rowsMax={4}
+    />
+  </div>
+);
 
 class CardForm extends React.Component {
   constructor(props) {
@@ -12,53 +32,63 @@ class CardForm extends React.Component {
   handleHintExpand() { this.setState({ hintExpanded: true }); }
 
   render() {
-    const { ...fields } = this.props;
+    const { fields } = this.props;
     return (
       <div>
-        <div className="input-row">
-          <TextField
-            id="card-question"
-            {...fields.names[0].input}
-            hintText={fields.hints[0]}
-            floatingLabelText={fields.labels[0]}
-            multiLine
-            fullWidth
-            rows={1}
-            rowsMax={4}
-          />
-        </div>
-        <div className="input-row">
-          <TextField
-            id="card-answer"
-            {...fields.names[1].input}
-            hintText={fields.hints[1]}
-            floatingLabelText={fields.labels[1]}
-            multiLine
-            fullWidth
-            rows={1}
-            rowsMax={4}
-          />
-        </div>
-        {this.state.hintExpanded ?
-          <div className="input-row">
-            <TextField
-              id="card-hint"
-              {...fields.names[2].input}
-              hintText={fields.hints[2]}
-              floatingLabelText={fields.labels[2]}
-              multiLine
-              fullWidth
-              rows={1}
-              rowsMax={4}
-            />
-          </div>
-        :
-          <FlatButton
-            label="Add hint"
-            fullWidth
-            onClick={this.handleHintExpand}
-          />
+        {fields.map((card, index) =>
+                (<Card key={['card', index].join('')}>
+                  <CardHeader
+                    title={`Card ${index + 1}`}
+                    showExpandableButton
+                    closeIcon={// eslint-disable-next-line
+                      <span onClick={() => fields.remove(index)}>
+                        <CancelIcon />
+                      </span>
+                    }
+                  />
+                  <CardText>
+                    <Field
+                      name={`${card}.question`}
+                      label="Question"
+                      type="text"
+                      component={renderField}
+                      hint="Ask for what?"
+                    />
+                    <Field
+                      name={`${card}.answer`}
+                      label="Answer"
+                      type="text"
+                      component={renderField}
+                      hint="What`s the answer?"
+                    />
+                    {this.state.hintExpanded ?
+                      <Field
+                        name={`${card}.hint`}
+                        label="Hint"
+                        type="text"
+                        component={renderField}
+                        hint="Some help?"
+                      />
+                        :
+                      <FlatButton
+                        label="Add hint"
+                        fullWidth
+                        onClick={this.handleHintExpand}
+                      />
+                    }{
+                    // eslint-disable-next-line
+                    }</CardText></Card>))
         }
+        <FlatButton
+          onClick={() => fields.push({})}
+          icon={<AddIcon color="white" />}
+          fullWidth
+          label="New card"
+          labelStyle={{ color: 'white' }}
+          labelPosition="after"
+          backgroundColor={green800}
+          hoverColor={green600}
+        />
       </div>
     );
   }
--- components/SetForm.jsx
+++ components/SetForm.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Fields, Field, reduxForm } from 'redux-form';
+import { FieldArray, Field, reduxForm } from 'redux-form';
 import TextField from 'material-ui/TextField';
-import { Card, CardTitle, CardText } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-import AddIcon from 'material-ui/svg-icons/content/add';
-import { green800, green600 } from 'material-ui/styles/colors';
+import { CardTitle } from 'material-ui/Card';
 import CardForm from '../components/CardForm';
 
 const setName = field => (
@@ -20,27 +17,7 @@ const setName = field => (
 );
 
 class SetForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { cardsNumber: 1, cardsArray: [] };
-    this.addCard = this.addCard.bind(this);
-  }
-
   componentDidMount() {
-  }
-
-  addCard() {
-    this.setState({ cardsNumber: this.state.cardsNumber + 1 });
-    const num = this.state.cardsNumber;
-    const cardInfo = this.state.cardsArray;
-    const questionName = ['cardQuestion', num].join('');
-    const answerName = ['cardAnswer', num].join('');
-    const hintName = ['cardHint', num].join('');
-    const key = ['card', num].join('');
-    cardInfo.push({
-      key, num, questionName, answerName, hintName,
-    });
-    this.setState({ cardsArray: cardInfo });
   }
 
   render() {
@@ -56,34 +33,11 @@ class SetForm extends React.Component {
             />
           </CardTitle>
           <div className="row">
-            {this.state.cardsArray &&
-              this.state.cardsArray.map((card, index) => (
-                <Card className="col-sm" key={card.key} number={card.num}>
-                  <CardText>
-                    <Fields
-                      number={index}
-                      names={[card.questionName, card.answerName, card.hintName]}
-                      hints={['Ask for what?', 'What`s the answer?', 'Some help?']}
-                      labels={['Question', 'Answer', 'Hint']}
-                      component={CardForm}
-                    />
-                  </CardText>{
-                  // eslint-disable-next-line
-                }</Card>
-              ))
-            }
+            <div className="col-sm">
+              <FieldArray name="card" component={CardForm} />
+            </div>
           </div>
         </div>
-        <FlatButton
-          onClick={this.addCard}
-          icon={<AddIcon color="white" />}
-          fullWidth
-          label="New card"
-          labelStyle={{ color: 'white' }}
-          labelPosition="after"
-          backgroundColor={green800}
-          hoverColor={green600}
-        />
       </form>
     );
   }
